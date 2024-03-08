bc0f35ab59fdb5b110bbb451d65769cee96c3f19
MhdMuzammilN <muzammil06n@gmail.com>
2024-03-08 13:58:01 +0530

    colors updated

--- lib/view/default_screen/default_screen.dart
+++ lib/view/default_screen/default_screen.dart
@@ -13,169 +13,206 @@ class _DefaultScreenState extends State<DefaultScreen> {
   int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //1. Body
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            title: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        color:
-                            ColorConstants.primaryShadeOfBlue.withOpacity(0.4),
-                        borderRadius: BorderRadius.circular(24)),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 24),
-                    child: const Row(
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          //1. Body
+          body: SafeArea(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  pinned: true,
+                  floating: true,
+                  snap: false,
+                  surfaceTintColor: Colors.white,
+                  title: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
                       children: <Widget>[
-                        Icon(
-                          Icons.search_rounded,
-                        ),
-                        SizedBox(
+                        Expanded(
+                            child: Container(
+                          decoration: BoxDecoration(
+                              color: ColorConstants.primaryShadeOfBlue
+                                  .withOpacity(0.4),
+                              borderRadius: BorderRadius.circular(24)),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 24),
+                          child: const Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.search_rounded,
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                "Search apps & games",
+                                style: TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Spacer(),
+                              Icon(Icons.mic_none),
+                            ],
+                          ),
+                        )),
+                        const SizedBox(
                           width: 15,
                         ),
-                        Text(
-                          "Search apps & games",
-                          style: TextStyle(
-                            fontSize: 13,
-                          ),
+                        const Stack(
+                          children: [
+                            Icon(Icons.notifications_none_outlined),
+                            Positioned(
+                              right: 0,
+                              top: 2,
+                              child: CircleAvatar(
+                                radius: 6,
+                                backgroundColor: ColorConstants.primaryBlue,
+                              ),
+                            )
+                          ],
                         ),
-                        Spacer(),
-                        Icon(Icons.mic_none),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        const CircleAvatar(
+                          radius: 14,
+                          backgroundImage:
+                              AssetImage(ImageConstants.userOneProfilePic),
+                        )
                       ],
                     ),
-                  )),
-                  const SizedBox(
-                    width: 15,
                   ),
-                  const Stack(
-                    children: [
-                      Icon(Icons.notifications_none_outlined),
-                      Positioned(
-                        right: 0,
-                        top: 2,
-                        child: CircleAvatar(
-                          radius: 6,
-                          backgroundColor: ColorConstants.primaryBlue,
+                  bottom: const TabBar(tabs: [
+                    Tab(
+                      text: "For you",
+                    ),
+                    Tab(
+                      text: "Top charts",
+                    ),
+                    Tab(
+                      text: "Kids",
+                    ),
+                    Tab(
+                      text: "Categories",
+                    ),
+                  ]),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Container(
+                        color: index.isOdd ? Colors.white : Colors.black12,
+                        height: 100.0,
+                        child: Center(
+                          child: Text('$index',
+                              textScaler: const TextScaler.linear(5)),
                         ),
-                      )
-                    ],
+                      );
+                    },
+                    childCount: 20,
                   ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const CircleAvatar(
-                    radius: 14,
-                    backgroundImage:
-                        AssetImage(ImageConstants.userOneProfilePic),
-                  )
-                ],
+                ),
+              ],
+            ),
+          ),
+          //2. BottomNavigationBar
+          bottomNavigationBar: BottomNavigationBar(
+            //1. Styling
+            type: BottomNavigationBarType.fixed,
+            iconSize: 26,
+            selectedItemColor: ColorConstants.primaryBlack,
+            selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            //2. Functional
+            currentIndex: currentPageIndex,
+            onTap: (value) {
+              currentPageIndex = value;
+              setState(() {});
+            },
+
+            //3. Navigation-Bar items
+            items: <BottomNavigationBarItem>[
+              //3.1. Games
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 14,
+                    ),
+                    decoration: BoxDecoration(
+                      color: currentPageIndex == 0
+                          ? ColorConstants.primaryShadeOfBlue
+                          : null,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: currentPageIndex == 0
+                        ? const Icon(Icons.sports_esports)
+                        : const Icon(Icons.sports_esports_outlined)),
+                label: "Games",
               ),
-            ),
-          )
-        ],
-      ),
-      //2. BottomNavigationBar
-      bottomNavigationBar: BottomNavigationBar(
-        //1. Styling
-        type: BottomNavigationBarType.fixed,
-        iconSize: 26,
-        selectedItemColor: ColorConstants.primaryBlack,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
 
-        //2. Functional
-        currentIndex: currentPageIndex,
-        onTap: (value) {
-          currentPageIndex = value;
-          setState(() {});
-        },
+              //3.2. Apps
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 14,
+                    ),
+                    decoration: BoxDecoration(
+                      color: currentPageIndex == 1
+                          ? ColorConstants.primaryShadeOfBlue
+                          : null,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: currentPageIndex == 1
+                        ? const Icon(Icons.grid_view_rounded)
+                        : const Icon(Icons.grid_view)),
+                label: "Apps",
+              ),
 
-        //3. Navigation-Bar items
-        items: <BottomNavigationBarItem>[
-          //3.1. Games
-          BottomNavigationBarItem(
-            icon: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 14,
+              //3.3. Offers
+              BottomNavigationBarItem(
+                icon: Transform.flip(
+                  flipX: true,
+                  child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 14,
+                      ),
+                      decoration: BoxDecoration(
+                        color: currentPageIndex == 2
+                            ? ColorConstants.primaryShadeOfBlue
+                            : null,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: currentPageIndex == 2
+                          ? const Icon(Icons.sell_rounded)
+                          : const Icon(Icons.sell_outlined)),
                 ),
-                decoration: BoxDecoration(
-                  color: currentPageIndex == 0
-                      ? ColorConstants.primaryShadeOfBlue
-                      : null,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: currentPageIndex == 0
-                    ? const Icon(Icons.sports_esports)
-                    : const Icon(Icons.sports_esports_outlined)),
-            label: "Games",
+                label: "Offers",
+              ),
+
+              //3.4 Books
+              BottomNavigationBarItem(
+                icon: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 14,
+                    ),
+                    decoration: BoxDecoration(
+                      color: currentPageIndex == 3
+                          ? ColorConstants.primaryShadeOfBlue
+                          : null,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: currentPageIndex == 3
+                        ? const Icon(Icons.book)
+                        : const Icon(Icons.book_outlined)),
+                label: "Books",
+              ),
+            ],
           ),
-
-          //3.2. Apps
-          BottomNavigationBarItem(
-            icon: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 14,
-                ),
-                decoration: BoxDecoration(
-                  color: currentPageIndex == 1
-                      ? ColorConstants.primaryShadeOfBlue
-                      : null,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: currentPageIndex == 1
-                    ? const Icon(Icons.grid_view_rounded)
-                    : const Icon(Icons.grid_view)),
-            label: "Apps",
-          ),
-
-          //3.3. Offers
-          BottomNavigationBarItem(
-            icon: Transform.flip(
-              flipX: true,
-              child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 14,
-                  ),
-                  decoration: BoxDecoration(
-                    color: currentPageIndex == 2
-                        ? ColorConstants.primaryShadeOfBlue
-                        : null,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: currentPageIndex == 2
-                      ? const Icon(Icons.sell_rounded)
-                      : const Icon(Icons.sell_outlined)),
-            ),
-            label: "Offers",
-          ),
-
-          //3.4 Books
-          BottomNavigationBarItem(
-            icon: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 14,
-                ),
-                decoration: BoxDecoration(
-                  color: currentPageIndex == 3
-                      ? ColorConstants.primaryShadeOfBlue
-                      : null,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: currentPageIndex == 3
-                    ? const Icon(Icons.book)
-                    : const Icon(Icons.book_outlined)),
-            label: "Books",
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
